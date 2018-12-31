7592aee9c917547b5eee7dad0775aed81335d482
Guy Whorley <guy.whorley@cherwell.com>
2018-12-30 23:25:12 -0800

    Pages wired up to list selection.

--- DucksAlive/pages/DucksAliveHome.xaml.cs
+++ DucksAlive/pages/DucksAliveHome.xaml.cs
@@ -28,7 +28,7 @@ namespace DucksAlive.pages
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
 			//View Duck Report
-			DucksAliveReportPage ducksAliveReportPage = new DucksAliveReportPage();
+			DucksAliveReportPage ducksAliveReportPage = new DucksAliveReportPage(duckListBox.SelectedItem);
 			NavigationService?.Navigate(ducksAliveReportPage);
 		}
 	}
